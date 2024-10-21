dce062f738ab0564853f85217a34f51dfe26dee3
ALThea1010 <164994153+ALThea1010@users.noreply.github.com>
2024-10-21 11:20:14 +0400

    Add files via upload

---
+++ Exercise 1.py
@@ -0,0 +1,13 @@
+"""
+Assessment1-Programming-Skills-Portfolio Exercise 1
+"""
+
+# Store words in variables
+word1 = "Coding"
+word2 = "is"
+word3 = "Cool"
+
+# Concatenate the words and print the phrase
+
+
+print(word1 + " " + word2 + " " + word3) # Output: Coding is Cool
